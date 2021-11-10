763506142107077d32c1ac6a0fdac3d99138e45c
harshit9715 <harshit9715@gmail.com>
2021-11-10 19:16:45 +0530

    Initial commit

---
+++ test/release-pipe-cdk-js.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as ReleasePipeCdkJs from '../lib/release-pipe-cdk-js-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/release-pipe-cdk-js-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReleasePipeCdkJs.ReleasePipeCdkJsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
