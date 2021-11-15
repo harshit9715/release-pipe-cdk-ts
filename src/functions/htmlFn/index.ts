9aa0d562c96e27e2104722e49dea648326a05afb
Harshit Gupta <harshit9715@gmail.com>
2021-11-15 19:06:33 +0530

    Update index.ts
    updated response

--- src/functions/htmlFn/index.ts
+++ src/functions/htmlFn/index.ts
@@ -1,6 +1,7 @@
 
 const handler = async function (event: any, context: any) {
-    const textColor = 'red';
+    const textColor = 'yellow';
+    const name = event?.queryStringParameters?.name || 'Stranger'
     
     return {
         statusCode: 200,
@@ -10,10 +11,13 @@ const handler = async function (event: any, context: any) {
         body: `<!doctype html>
                 <html>
                     <head>
-                        <title>My Demo App </title>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" crossorigin="anonymous">
+                        <title>My Demo App</title>
                     </head>
                     <body style="background-color: #0336FF;">
-                        <h1 style="color:${textColor};">Hello, world!</h1>
+                        <h1 style="color:${textColor};">Hello, ${name}!</h1>
                     </body>
                 </html>`,
     };
